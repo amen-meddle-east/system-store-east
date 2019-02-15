a6bd0f88633e1a7c204ed354f04aa6b971f59ba2
amen-meddle-east <47331868+amen-meddle-east@users.noreply.github.com>
2019-02-15 18:30:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -275,29 +275,6 @@ client.on('message', message => {   //اخفاء الروم
                   message.channel.send('تم ')
      }
     });
-	
-	
-	
-	  client.on('message', omar => {
-						
-                        if(omar.content.split(' ')[0] == adminprefix + 'dc') {  // delete all channels
-                        if (!omar.channel.guild) return;
-                        if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
-                        if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
-                        omar.guild.channels.forEach(m => {
-                        m.delete();
-                        });
-                        }
-                        if(omar.content.split(' ')[0] == adminprefix + 'dr') { // delete all roles
-                        if (!omar.channel.guild) return;
-                        if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES")) return omar.reply("**You Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-                        if(!omar.guild.member(client.user).hasPermission("MANAGE_ROLES")) return omar.reply("**I Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-                        omar.guild.roles.forEach(m => {
-                        m.delete();
-                        });
-                        omar.reply("`تم حذف جميع الرتب بنجاح`")
-                        }
-                        });////
 						
 						
 						
